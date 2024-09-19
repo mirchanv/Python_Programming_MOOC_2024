150f7abfc8eea420368519ccaafbe66a7ba164a2
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2024-09-20 00:34:34 +0200

    Add files via upload

---
+++ Part 1/1.8 Name and Address/src/name_and_address.py
@@ -0,0 +1,9 @@
+# Write your solution here
+first_name = input("Given name: ")
+family_name = input("Family name: ")
+street_address = input("Street address: ")
+city_postal_code = input("City and postal code: ")
+
+print(first_name + " " + family_name)
+print(street_address)
+print(city_postal_code)
