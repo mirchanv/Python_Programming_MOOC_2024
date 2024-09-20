4375a3ec98b80eb57488c9e5f340713f50af2e88
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2024-09-20 12:19:24 +0200

    Add files via upload

---
+++ Part 1/15 Seconds in a Day/src/seconds_in_a_day.py
@@ -0,0 +1,5 @@
+# Write your solution here
+days = int(input("How many days? "))
+
+# One minute has 60 seconds, One hour has 60 minutes and one day has 24 hours
+print(f"Seconds in that many days: {60*60*24*days}")
---
+++ Part 1/16 Product/src/product.py
@@ -0,0 +1,9 @@
+# Fix the code
+number = int(input("Please type in the first number: "))
+product = number;
+number = int(input("Please type in the second number: "))
+product *= number;
+number = int(input("Please type in the third number: "))
+product *= number;
+
+print("The product is", product)
