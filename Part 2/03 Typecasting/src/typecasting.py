5be90fdd72e53dd7bfe2ed3f608385b51406ed31
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2024-09-21 00:05:16 +0200

    Add files via upload

---
+++ Part 2/03 Typecasting/src/typecasting.py
@@ -0,0 +1,8 @@
+# Write your solution here
+num = float(input("Please type in a number: "))
+
+integer_part = int(num)
+print(f"Integer part: {integer_part}")
+
+decimal_part = num % 1
+print(f"Decimal part: {decimal_part}")
---
+++ Part 2/05 Greater or Equal/src/greater_or_equal.py
@@ -0,0 +1,10 @@
+# Write your solution here
+first_num = int(input("Please type in the first number: "))
+second_num = int(input("Please type in another number: "))
+
+if first_num > second_num:
+    print(f"The greater number was {first_num}")
+elif second_num > first_num:
+    print(f"The greater number was {second_num}")
+else: 
+    print("The numbers are equal!")
